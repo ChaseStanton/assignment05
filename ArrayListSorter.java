c99c1a55f5c61281df1c18e7c939348cc02c8491
ChaseStanton <stantonchase4217@gmail.com>
2023-10-01 18:49:11 -0600

    Update ArrayListSorter.java

--- ArrayListSorter.java
+++ ArrayListSorter.java
@@ -1,5 +1,16 @@
 package assign05;
 
+import java.util.ArrayList;
+
 public class ArrayListSorter {
-//dsfsdaf
+	public static <T extends Comparable<? super T>> void mergesort(ArrayList<T> list) {
+		
+	}
+	public static <T extends Comparable<? super T>> void quicksort(ArrayList<T> list) {
+		
+	}
+
+
+
+
 }
