0a7758b6aa7ad82ee2c7ffb59342619c9df8c32f
Reece Kalmar <ralmarkalmar@gmail.com>
2023-10-01 18:50:56 -0600

    ds

--- ArrayListSorter.java
+++ ArrayListSorter.java
@@ -9,8 +9,16 @@ public class ArrayListSorter {
 	public static <T extends Comparable<? super T>> void quicksort(ArrayList<T> list) {
 		
 	}
+    public static ArrayList<Integer> generateAscending(int size){
+        return new ArrayList<Integer>();
+    }
+    public static ArrayList<Integer> generatePermuted(int size){
+        return new ArrayList<Integer>();
 
+    }
+    public static ArrayList<Integer> generateDescending(int size){
+        return new ArrayList<Integer>();
 
-
+    }
 
 }
