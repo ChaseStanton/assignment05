53fa1b1cb1665bdba51fd68247a8b148a1acfb09
ChaseStanton <stantonchase4217@gmail.com>
2023-10-01 19:12:49 -0600

    Update ArrayListSorter.java

--- ArrayListSorter.java
+++ ArrayListSorter.java
@@ -6,6 +6,21 @@ public class ArrayListSorter {
 	public static <T extends Comparable<? super T>> void mergesort(ArrayList<T> list) {
 		
 	}
+	private static<T extends Comparable<? super T>> void merge(ArrayList<T> list, ArrayList<T> left, ArrayList<T> right) {
+		int i = 0, j = 0, k = 0;
+		while(i < left.size() && j < right.size()) {
+			if(left.get(i).compareTo(right.get(j)) < 0)
+				list.set(k++, left.get(i++));
+			else
+				list.set(k++, right.get(j++));
+
+		}
+		while( i < left.size())
+			list.set(k++, left.get(i++));
+		while(j < right.size())
+			list.set(k++, right.get(j++));
+
+	}
 	public static <T extends Comparable<? super T>> void quicksort(ArrayList<T> list) {
 		
 	}
