525e4aa5422f0f0f52ba41dbfb44a33d02e3829b
ChaseStanton <stantonchase4217@gmail.com>
2023-10-01 19:23:54 -0600

    Update ArrayListSorterTest.java

--- ArrayListSorterTest.java
+++ ArrayListSorterTest.java
@@ -1,5 +1,30 @@
 package assign05;
 
-public class ArrayListSorterTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ArrayListSorterTest {
+private ArrayList<Integer> intList;
+	@BeforeEach
+	void setUp() throws Exception {
+		intList = new ArrayList<>();
+		intList.add(1);
+		intList.add(-13);
+		intList.add(20);
+		intList.add(0);
+		intList.add(7);
+	}
+
+	@Test
+	void testMergeSort() {
+		ArrayListSorter.mergesort(intList);
+		assertEquals(-13, intList.get(0));
+		assertEquals(1, intList.get(2));
+		assertEquals(20, intList.get(4));
+	}
 
 }
