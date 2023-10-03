304f5a1d6721b6713359cb9abb2c53000918c664
ChaseStanton <stantonchase4217@gmail.com>
2023-10-03 17:52:59 -0600

    efbhj

--- ArrayListSorter.java
+++ ArrayListSorter.java
@@ -2,69 +2,91 @@ package assign05;
 
 import java.util.ArrayList;
 
-public class ArrayListSorter {
-    private static final int insertionSortThreshold = 10;
+public class ArrayListSorter<T extends Comparable<? super T>> {
+	private static int insertionSortThreshold = 10;
 
-    public static <T extends Comparable<? super T>> void mergesort(ArrayList<T> list) {
-        int size = list.size();
-        if (size < insertionSortThreshold) {
-            insertionSort(list);
-        } else {
-            ArrayList<T> leftSide = new ArrayList<>(list.subList(0, size / 2));
-            ArrayList<T> rightSide = new ArrayList<>(list.subList(size / 2, size));
-            mergesort(leftSide);
-            mergesort(rightSide);
+	public static <T extends Comparable<? super T>> void mergesort(ArrayList<T> list) {
+		ArrayList<T> temp = new ArrayList<T>(list);
+		mergesort(list, temp, 0, list.size() - 1);
+	}
 
-            merge(list, leftSide, rightSide);
-        }
-    }
+	private static <T extends Comparable<? super T>> void mergesort(ArrayList<T> list, ArrayList<T> temp, int left,
+			int right) {
+		if (left < right) {
+			if (right - left < insertionSortThreshold) {
+				insertionSort(list, left, right);
+			} else {
 
-    private static <T extends Comparable<? super T>> void insertionSort(ArrayList<T> list) {
-        for (int i = 1; i < list.size(); i++) {
-            T current = list.get(i);
-            int before = i - 1;
+				int mid = (left + right) / 2;
+				mergesort(temp, list, left, mid);
+				mergesort(temp, list, mid + 1, right);
+				merge(list, temp, left, mid, right);
+			}
+		}
 
-            while (before >= 0 && list.get(before).compareTo(current) > 0) {
-                list.set(before + 1, list.get(before));
-                before--;
-            }
-            list.set(before + 1, current);
-        }
-    }
+	}
 
-    private static <T extends Comparable<? super T>> void merge(ArrayList<T> list, ArrayList<T> left,
-            ArrayList<T> right) {
-        int i = 0, j = 0, k = 0;
-        while (i < left.size() && j < right.size()) {
-            if (left.get(i).compareTo(right.get(j)) < 0)
-                list.set(k++, left.get(i++));
-            else
-                list.set(k++, right.get(j++));
+	private static <T extends Comparable<? super T>> void insertionSort(ArrayList<T> list, int left, int right) {
+		for (int i = left + 1; i <= right; i++) {
+			T current = list.get(i);
+			int before = i - 1;
 
-        }
-        while (i < left.size())
-            list.set(k++, left.get(i++));
-        while (j < right.size())
-            list.set(k++, right.get(j++));
+			while (before >= left && list.get(before).compareTo(current) > 0) {
+				list.set(before + 1, list.get(before));
+				before--;
+			}
+			list.set(before + 1, current);
+		}
+	}
 
-    }
+	private static <T extends Comparable<? super T>> void merge(ArrayList<T> list, ArrayList<T> temp, int left, int mid,
+			int right) {
+		int i = left;
+		int j = mid + 1;
 
-    public static <T extends Comparable<? super T>> void quicksort(ArrayList<T> list) {
-    	 
-    	 }
-    
-    public static ArrayList<Integer> generateAscending(int size) {
-        return new ArrayList<Integer>();
-    }
+		for (int k = left; k <= right; k++) {
+			if (i > mid) {
+				list.set(k, temp.get(j++));
+			} else if (j > right) {
+				list.set(k, temp.get(i++));
+			} else if (temp.get(i).compareTo(temp.get(j)) <= 0) {
+				list.set(k, temp.get(i++));
+			} else {
+				list.set(k, temp.get(j++));
+			}
+		}
+	}
 
-    public static ArrayList<Integer> generatePermuted(int size) {
-        return new ArrayList<Integer>();
+	public static <T extends Comparable<? super T>> void quicksort(ArrayList<T> list) {
 
-    }
+	}
 
-    public static ArrayList<Integer> generateDescending(int size) {
-        return new ArrayList<Integer>();
+	public static ArrayList<Integer> generateAscending(int size) {
+		return new ArrayList<Integer>();
+	}
 
-    }
+	public static ArrayList<Integer> generatePermuted(int size) {
+		return new ArrayList<Integer>();
+
+	}
+
+	public static ArrayList<Integer> generateDescending(int size) {
+		return new ArrayList<Integer>();
+
+	}
+
+	public static void main(String[] args) {
+		ArrayList<Integer> intList = new ArrayList<>();
+		intList.add(3);
+		intList.add(1);
+		intList.add(4);
+		intList.add(1);
+		intList.add(5);
+		intList.add(9);
+
+		ArrayListSorter.mergesort(intList);
+
+		System.out.println("Sorted list: " + intList);
+	}
 
 }
--- ArrayListSorterTest.java
+++ ArrayListSorterTest.java
@@ -15,6 +15,11 @@ private ArrayList<Integer> intList;
 		intList.add(1);
 		intList.add(-13);
 		intList.add(20);
+		
+	}
+
+	@Test
+	void testMergeSort() {
 		intList.add(0);
 		intList.add(7);
 		intList.add(100);
@@ -24,16 +29,19 @@ private ArrayList<Integer> intList;
 		intList.add(22);
 		intList.add(30);
 		intList.add(-3);
-	}
-
-	@Test
-	void testMergeSort() {
 		ArrayListSorter.mergesort(intList);
 		assertEquals(-50, intList.get(0));
 		assertEquals(-3, intList.get(2));
 		assertEquals(1, intList.get(4));
 		assertEquals(1, intList.get(4));
 
+	}
+	@Test
+	void testSmallList() {
+		ArrayListSorter.mergesort(intList);
+		assertEquals(-13, intList.get(0));
+		assertEquals(1, intList.get(1));
+		assertEquals(20, intList.get(2));
 	}
 
 }
